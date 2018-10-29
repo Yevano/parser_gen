ba0557007c16bb8f76f37dc789c3cb4615ed803a
Yevano <jamking1996@gmail.com>
2018-10-28 21:38:59 -0700

    Use fresh variables

--- src/peg.cr
+++ src/peg.cr
@@ -24,58 +24,69 @@ end
 
 macro parser_expr(ast)
     {% if ast.class_name == "RegexLiteral" %}
-        Parser.new ->(str : String) {
-            md = {{ ast }}.match(str)
-            puts "asdasda"
-            puts md ? md.byte_end : -1
-            puts "asdasd"
-            if md && md.begin == 0
-                ParseResult.new(true, md.byte_end, md[0])
+        Parser.new ->(%str : String) {
+            %md = {{ ast }}.match(%str)
+            if %md && %md.begin == 0
+                ParseResult.new(true, %md.byte_end, %md[0])
             else
                 ParseResult.new(false)
             end
         }
     {% elsif ast.class_name == "Call" %}
         {% if ast.name == "|" %}
-            Parser.new ->(str : String) {
-                pr = parser_expr({{ ast.receiver }}).parse(str)
-                if pr.success
-                    pr
+            Parser.new ->(%str : String) {
+                %pr = parser_expr({{ ast.receiver }}).parse(%str)
+                if %pr.success
+                    %pr
                 else
-                    parser_expr({{ ast.args[0] }}).parse(str)
+                    parser_expr({{ ast.args[0] }}).parse(%str)
                 end
             }
         {% elsif ast.name == ">>" %}
-            Parser.new ->(str : String) {
-                pr = parser_expr({{ ast.receiver }}).parse(str)
-                if pr.success
-                    next_pr = parser_expr({{ ast.args[0] }}).parse(str[pr.byte_end..-1])
-                    if next_pr.success
-                        ParseResult.new(true, next_pr.byte_end, str[0...pr.byte_end + next_pr.byte_end])
+            Parser.new ->(%str : String) {
+                %pr = parser_expr({{ ast.receiver }}).parse(%str)
+                if %pr.success
+                    %next_pr = parser_expr({{ ast.args[0] }}).parse(%str[%pr.byte_end..-1])
+                    if %next_pr.success
+                        ParseResult.new(true, %pr.byte_end + %next_pr.byte_end, %str[0...%pr.byte_end + %next_pr.byte_end])
                     else
-                        next_pr
+                        %next_pr
                     end
                 else
-                    pr
+                    %pr
                 end
             }
+        {% elsif ast.name == "rep" %}
+            Parser.new ->(%str : String) {
+                %string = String.build { |%string|
+                    %pr : ParseResult
+                    loop do
+                        %pr = parser_expr({{ ast.receiver }}).parse(%str)
+                        break unless %pr.success
+                        %string << %pr.parsed
+                        %str = %str[%pr.byte_end..-1]
+                    end
+                }
+                ParseResult.new(true, %string.size(), %string)
+            }
         {% end %}
+    {% elsif ast.class_name == "SymbolLiteral" %}
+        rules["{{ ast.id }}"]
     {% elsif ast.class_name == "Expressions" %}
         parser_expr({{ ast.expressions[0] }})
     {% end %}
 end
 
 macro parser(ast)
-    {{ puts ast.class_name }}
     {% if ast.class_name == "NamedTupleLiteral" %}
         Parser.new ->(str : String) {
-            rules = Hash(String, (String -> ParseResult)).new
+            rules = Hash(String, Parser).new
             {% for key, value in ast %}
-                rules["{{ key.id }}"] = ->(str : String) {
+                rules["{{ key.id }}"] = Parser.new ->(str : String) {
                     parser_expr({{ value }}).parse(str)
                 }
             {% end %}
-            rules["main"].call(str)
+            rules["main"].parse(str)
         }
     {% end %}
 end
