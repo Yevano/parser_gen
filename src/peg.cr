dad8ff1c5186752e63330f641b0a2f85e9ba7633
Yevano <jamking1996@gmail.com>
2018-10-30 13:29:02 -0700

    Add let binding for grammar expressions

--- src/peg.cr
+++ src/peg.cr
@@ -1,9 +1,26 @@
 class ParseResult
     property success : Bool
+    property label : Symbol?
+    property children : Set(ParseResult)
     property byte_end : Int32
-    property parsed : String?
+    property parsed : String
 
-    def initialize(@success, @byte_end = 0, @parsed = nil)
+    def initialize(@success, @children = Set(ParseResult).new, @byte_end = 0, @parsed = "")
+    end
+
+    def var(sym : Symbol) : ParseResult
+        if @label == sym
+            self
+        else
+            elem = @children.find { |e|
+                e.var(sym)
+            }
+            if elem
+                elem.var(sym)
+            else
+                raise "Could not find var #{sym}."
+            end
+        end
     end
 
     def to_s(io)
@@ -27,7 +44,7 @@ macro parser_expr(ast)
         Parser.new ->(%str : String) {
             %md = {{ ast }}.match(%str)
             if %md && %md.begin == 0
-                ParseResult.new(true, %md.byte_end, %md[0])
+                ParseResult.new(true, Set(ParseResult).new, %md.byte_end, %md[0])
             else
                 ParseResult.new(false)
             end
@@ -48,7 +65,7 @@ macro parser_expr(ast)
                 if %pr.success
                     %next_pr = parser_expr({{ ast.args[0] }}).parse(%str[%pr.byte_end..-1])
                     if %next_pr.success
-                        ParseResult.new(true, %pr.byte_end + %next_pr.byte_end, %str[0...%pr.byte_end + %next_pr.byte_end])
+                        ParseResult.new(true, Set { %pr, %next_pr }, %pr.byte_end + %next_pr.byte_end, %str[0...%pr.byte_end + %next_pr.byte_end])
                     else
                         %next_pr
                     end
@@ -58,16 +75,18 @@ macro parser_expr(ast)
             }
         {% elsif ast.name == "rep" %}
             Parser.new ->(%str : String) {
+                %ch = Set(ParseResult).new
                 %string = String.build { |%string|
                     %pr : ParseResult
                     loop do
                         %pr = parser_expr({{ ast.receiver }}).parse(%str)
                         break unless %pr.success
+                        %ch.add(%pr)
                         %string << %pr.parsed
                         %str = %str[%pr.byte_end..-1]
                     end
                 }
-                ParseResult.new(true, %string.size(), %string)
+                ParseResult.new(true, %ch, %string.size(), %string)
             }
         {% elsif ast.name == "maybe" %}
             Parser.new ->(%str : String) {
@@ -75,8 +94,26 @@ macro parser_expr(ast)
                 if %pr.success
                     %pr
                 else
-                    ParseResult.new(true, 0, "")
+                    ParseResult.new(true, Set(ParseResult).new, 0, "")
                 end
+            }
+        {% elsif ast.name == "on" %}
+            Parser.new ->(%str : String) {
+                %pr = parser_expr({{ ast.receiver }}).parse(%str)
+                if %pr.success
+                    {% if ast.args.size == 1 %}
+                        {{ ast.args[0] }}(%pr)
+                    {% else %}
+                        ->({{ ast.block.args[0] }} : ParseResult) { {{ ast.block.body }} }.call(%pr)
+                    {% end %}
+                end
+                %pr
+            }
+        {% elsif ast.name == "let" %}
+            Parser.new ->(%str : String) {
+                %pr = parser_expr({{ ast.receiver }}).parse(%str)
+                %pr.label = {{ ast.args[0] }}
+                %pr
             }
         {% end %}
     {% elsif ast.class_name == "SymbolLiteral" %}
